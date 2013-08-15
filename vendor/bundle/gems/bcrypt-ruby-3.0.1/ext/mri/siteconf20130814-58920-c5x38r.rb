78244f5864687582ecfffd399d928c40421a4093
kossgreim <ikossgreim@gmail.com>
2013-08-15 14:06:57 +0300

    reset

---
+++ vendor/bundle/gems/bcrypt-ruby-3.0.1/ext/mri/siteconf20130814-58920-c5x38r.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130814-58920-15p35p2"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
